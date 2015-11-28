4e663da11746d5aa5b87fa0307bd3c153dad0128
Sebastiaan Luca <hello@sebastiaanluca.com>
2015-11-28 20:26:07 +0100

    Better debug messages

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var debug = require('debug');
+var debug = require('debug')('app');
 
 var fs = require('fs');
 var request = require('request');
@@ -13,14 +13,6 @@ var Player = require('player');
 //loudness.setVolume(50, function (err) {
 //    console.log('Set volume to 50%');
 //});
-//
-//fs.createReadStream('resources/audio/audio.mp3')
-//.pipe(new lame.Decoder())
-//.on('format', function (format) {
-//    console.log('Playing audio file');
-//    
-//    this.pipe(new Speaker(format));
-//});
 
 var playlist = 'http://stream.boosh.fm/booshfm_mp3256.pls';
 
@@ -28,13 +20,13 @@ request.get(playlist, function (error, response, body) {
     
     // Error handling
     if (error) {
-        console.log(error);
+        debug('Error loading %s (error)', playlist, response.statusCode);
         
         return;
     }
     
     if (response.statusCode != 200) {
-        console.log(response.statusCode);
+        debug('Error loading %s (status code)', playlist, response.statusCode);
         
         return;
     }
@@ -43,7 +35,7 @@ request.get(playlist, function (error, response, body) {
     var streams = pls.parse(body);
     var source = streams[0].file;
     
-    console.log('Found a playable stream: ' + source);
+    debug('Found a playable stream: ' + source);
     
     // Play stream
     var player = new Player([source]);
@@ -51,29 +43,18 @@ request.get(playlist, function (error, response, body) {
     player.enable('stream');
     player.play();
     
-    player.on('playing',function(item){
-        console.log('im playing... src:' + item);
+    player.on('playing', function (item) {
+        debug('Playing %s', item.src);
     });
     
-    // event: on playend 
-    player.on('playend',function(item){
-        // return a playend item 
-        console.log('src:' + item + ' play done, switching to next one ...');
+    player.on('playend', function (item) {
+        debug('Stopped playing %s', item.src);
+        
+        debug('Stopping playback');
+        player.stop();
     });
     
     player.on('error', function (err) {
-        console.log('PLAYER ERROR', err);
+        debug('Error playing source', err);
     });
-    
-//    new Player('http://stream.srg-ssr.ch/m/rsp/mp3_128')
-//    .enable('stream')
-//    .on('playing', function(song) {
-//        debug('Playing... ');
-//        debug(song);
-//    })
-//    .on('error', function(err) {
-//        debug('Opps...!');
-//        debug(err);
-//    })
-//    .play()
 });
